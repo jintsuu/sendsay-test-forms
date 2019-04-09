24823f559958486b29c2daf3fc0aab44c00639d1
Nikita Panasovich <nipanasovich@gmail.com>
2019-04-09 21:10:05 +0300

    Updates

---
+++ precache-manifest.604e75439041bffd95c4dfe4917e57a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd8b42e2d7936ca62fe16b6384c1b571",
+    "url": "/sendsay-test-forms/static/media/del.fd8b42e2.svg"
+  },
+  {
+    "revision": "457e9b259a71d30c9978",
+    "url": "/sendsay-test-forms/static/js/runtime~main.2bc01a5e.js"
+  },
+  {
+    "revision": "692507619284725aca53",
+    "url": "/sendsay-test-forms/static/js/main.52500e06.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/sendsay-test-forms/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "692507619284725aca53",
+    "url": "/sendsay-test-forms/static/css/main.d385864b.chunk.css"
+  },
+  {
+    "revision": "85c0d742e76eab4a0aced559b92d62e3",
+    "url": "/sendsay-test-forms/index.html"
+  }
+];
